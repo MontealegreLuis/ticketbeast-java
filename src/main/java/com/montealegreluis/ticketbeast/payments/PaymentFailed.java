68962e373ec46640fdf4d24954be173c7107b193
Luis Montealegre <montealegreluis@gmail.com>
2022-04-03 12:04:42 -0500

    fix: `PaymentFailed` extends `InvalidAction`

--- src/main/java/com/montealegreluis/ticketbeast/payments/PaymentFailed.java
+++ src/main/java/com/montealegreluis/ticketbeast/payments/PaymentFailed.java
@@ -1,8 +1,8 @@
 package com.montealegreluis.ticketbeast.payments;
 
-import com.montealegreluis.servicebuses.DomainException;
+import com.montealegreluis.ticketbeast.shared.InvalidAction;
 
-public final class PaymentFailed extends DomainException {
+public final class PaymentFailed extends InvalidAction {
   public static PaymentFailed forTransactionWith(PaymentToken token) {
     return new PaymentFailed("Payment with token '" + token.value() + "' cannot be completed");
   }
